588844347f4fe84244c0e7815df47514c38276a8
nikitagorstukov <n.gorstukov@gmail.com>
2022-09-14 18:39:55 +0200

    First task

--- javal.essons/src/main/java/school/lesson1/HomeWorkApp.java
+++ javal.essons/src/main/java/school/lesson1/HomeWorkApp.java
@@ -1,4 +1,25 @@
 package school.lesson1;
 
 public class HomeWorkApp {
+    public static void main (String [] args) {
+            printThreeWords();
+            checkSumSign();
+        }
+        public static void printThreeWords() {
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+        }
+
+        public static void checkSumSign() {
+            int a = 10;
+            int b = 5;
+            int sum = a + b;
+
+            if (sum >= 0) {
+                System.out.println("Сумма положительная");
+            } else {
+                System.out.println("Сумма отрицательная");
+            }
+    }
 }
