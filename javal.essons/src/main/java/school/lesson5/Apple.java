c3bcf2a3a3295e3a5576fe2a359a3e4769db53d6
nikitagorstukov <n.gorstukov@gmail.com>
2022-09-27 16:47:37 +0200

    Lesson 5

---
+++ javal.essons/src/main/java/school/lesson5/Apple.java
@@ -0,0 +1,13 @@
+package school.lesson5;
+
+public class Apple extends Fruit {
+
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "яблоки";
+    }
+}
